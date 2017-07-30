7e31762362b465689f673f688fe477c8571e1ab3
Kamran <kamran.mujtaba@hotmail.com>
2017-07-30 11:02:55 +0100

    _.indexOf implemented and tested

--- lowbar.js
+++ lowbar.js
@@ -12,7 +12,7 @@ _.first = function (arr, num) {
 
 _.last = function (arr, num) {
     if (arguments.length === 1 || num === 1) return arr[arr.length - 1];
-     return arr.slice(-num);
+    return arr.slice(-num);
 };
 
 _.each = function (list, func) {
@@ -26,6 +26,26 @@ _.each = function (list, func) {
         }
     }
     return list;
+};
+
+_.indexOf = function (arr, num, bool) {
+    if (arguments.length < 3 || !bool) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === num) return i;
+        }
+        return -1;
+    }
+    if (bool) {
+        return binarySearch(arr, num);
+    }
+
+    function binarySearch(arr, num) {
+        var midPoint = Math.floor(arr.length - 1 / 2);
+        if (arr[midPoint] > num) return binarySearch(arr.slice(0, midPoint), num);
+        else if (arr[midPoint] < num) return binarySearch(arr.slice(midPoint + 1, arr.length - 1), num);
+        else return midPoint;
+    }
+
 };
 
 if (typeof module !== 'undefined') {
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -92,4 +92,26 @@ describe('_', function() {
         });
 
     });
+    // indexOf method
+    describe('#indexOf', function () {
+        it('is a function', function () {
+            expect(_.indexOf).to.be.a('function');
+        });
+        it('it should return index if found otherwise -1', function () {
+            var result = _.indexOf([1, 2, 3, 4, 5], 3);
+            expect(result).to.eql(2);
+        });
+        it('it should return -1', function () {
+            var result = _.indexOf([1, 2, 3, 4, 5], 7);
+            expect(result).to.eql(-1);
+        });
+        it('it should return index if found otherwise -1', function () {
+            var result = _.indexOf([1, 2, 3, 4, 5], 1);
+            expect(result).to.eql(0);
+        });
+        it('it should return index if found and start from given index if third argument is passed', function () {
+            var result = _.indexOf([1, 2, 3, 4, 5, 1], 1, 2);
+            expect(result).to.eql(5);
+        });
+    });
 });
