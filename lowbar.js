0e4030caa148daf61f4c9e3410e8661f67949d71
Kamran <kamran.mujtaba@hotmail.com>
2017-07-30 11:25:10 +0100

    _.contains implemented and tested

--- lowbar.js
+++ lowbar.js
@@ -102,6 +102,19 @@ _.map = function (arr, func) {
     return result;
 };
 
+// contains method
+_.contains = function (arr, num) {
+    if (Array.isArray(arr)) {
+        if (arr.indexOf(num) > -1) return true;
+        return false;
+    }
+    else if (typeof (arr) === 'object') {
+
+        if (Object.values(arr).indexOf(num) > -1) { return true; }
+        return false;
+    }
+};
+
 if (typeof module !== 'undefined') {
     module.exports = _;
 }
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -233,4 +233,41 @@ describe('_', function () {
             expect(result).to.eql(['$3', '$5', '$1']);
         });
     });
+    // contains method
+    describe('#contains', function () {
+
+        it('is a function', function () {
+            expect(_.contains).to.be.a('function');
+        });
+        it('should return boolean', function () {
+            var result = _.contains([1, 2, 3, 4, 5], 3);
+            expect(result).to.be.an('boolean');
+        });
+        it('Should return true if value is found in array', function () {
+            var result = _.contains([1, 2, 3, 4, 5], 1);
+            expect(result).to.eql(true);
+        });
+        it('Should return false if value not found in array', function () {
+            var result = _.contains([1, 2, 3], 4);
+            expect(result).to.eql(false);
+        });
+        it('Should return true if value found in object', function () {
+
+            var result = _.contains({
+                a: 3,
+                b: 5,
+                c: 1
+            }, 3);
+            expect(result).to.eql(true);
+        });
+        it('Should return false if value not found in object', function () {
+
+            var result = _.contains({
+                a: 3,
+                b: 5,
+                c: 1
+            }, 2);
+            expect(result).to.eql(false);
+        });
+    });
 });
