2e026d8aec53e21d098e158fce94fd8abd402eaa
Kamran <kamran.mujtaba@hotmail.com>
2017-07-30 11:23:24 +0100

    _.map implemented and tested

--- lowbar.js
+++ lowbar.js
@@ -86,6 +86,22 @@ _.uniq = function (arr) {
     return result;
 };
 
+// map method
+_.map = function (arr, func) {
+    var result = [];
+    if (Array.isArray(arr)) {
+        for (var i = 0; i < arr.length; i++) {
+            result.push(func(arr[i]));
+        }
+    } else if (typeof (arr) === 'object') {
+
+        for (var key in arr) {
+            result.push(func(arr[key]));
+        }
+    }
+    return result;
+};
+
 if (typeof module !== 'undefined') {
     module.exports = _;
 }
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -199,4 +199,38 @@ describe('_', function () {
             expect(result).to.eql(['a', 'b', 'c']);
         });
     });
+    // Map method
+    describe('#map', function () {
+        function multBy3(num) {
+            return num * 3;
+        }
+
+        function addDollar(num) {
+            return '$' + num;
+        }
+        it('is a function', function () {
+            expect(_.map).to.be.a('function');
+        });
+        it('should return an array', function () {
+            var result = _.map([1, 2, 3, 4, 5], multBy3);
+            expect(result).to.be.an('array');
+        });
+        it('map out a new array it\'s elements equal to input elements array multiplied by 3', function () {
+            var result = _.map([1, 2, 3, 4, 5], multBy3);
+            expect(result).to.eql([3, 6, 9, 12, 15]);
+        });
+        it('map out a new array containnig dollar character', function () {
+            var result = _.map([1, 2, 3], addDollar);
+            expect(result).to.eql(['$1', '$2', '$3']);
+        });
+        it('map out a new array from object', function () {
+
+            var result = _.map({
+                a: 3,
+                b: 5,
+                c: 1
+            }, addDollar);
+            expect(result).to.eql(['$3', '$5', '$1']);
+        });
+    });
 });
