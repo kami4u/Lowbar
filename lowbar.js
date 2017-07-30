b01c4b9c5de59460fb6a8a4d32210b5bb3c2c7f1
Kamran <kamran.mujtaba@hotmail.com>
2017-07-30 10:38:54 +0100

    _.last implemented and tested

--- lowbar.js
+++ lowbar.js
@@ -6,9 +6,15 @@ _.identity = function (arg) {
 
 _.first = function (arr, num) {
     if (!Array.isArray(arr)) return;
-    if (arguments.length === 1 || num === 1) { return arr[0]; }
+    if (arguments.length === 1 || num === 1) return arr[0];
     return arr.slice(0, num);
 };
+
+_.last = function (arr, num) {
+    if (arguments.length === 1 || num === 1) return arr[arr.length - 1];
+     return arr.slice(-num);
+};
+
 
 if (typeof module !== 'undefined') {
     module.exports = _;
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -35,4 +35,22 @@ describe('_', function() {
             expect(result).to.eql([{ name: 'kami' }, { name: 'wassan' }]);
         });
     });
+    // last method
+    describe('#last', function () {
+        it('is a function', function () {
+            expect(_.last).to.be.a('function');
+        });
+        it('it should return last element of array if num is undefined', function () {
+            var result = _.last([1, 2, 3, 4, 5]);
+            expect(result).to.eql(5);
+        });
+        it('it should return last nth elements of array', function () {
+            var result = _.last([1, 2, 3, 4, 5], 2);
+            expect(result).to.eql([4, 5]);
+        });
+        it('it should return last nth elements of array', function () {
+            var result = _.last([{ name: 'kami' }, { name: 'wassan' }, { name: 'men' }], 2);
+            expect(result).to.eql([{ name: 'wassan' }, { name: 'men' }]);
+        });
+    });
 });
