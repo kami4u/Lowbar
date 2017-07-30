7a02089826230641fd956103ad361d9aadb9de41
Kamran <kamran.mujtaba@hotmail.com>
2017-07-30 10:20:20 +0100

    _.first implemented and tested

--- lowbar.js
+++ lowbar.js
@@ -4,6 +4,12 @@ _.identity = function (arg) {
     return arg;
 };
 
+_.first = function (arr, num) {
+    if (!Array.isArray(arr)) return;
+    if (arguments.length === 1 || num === 1) { return arr[0]; }
+    return arr.slice(0, num);
+};
+
 if (typeof module !== 'undefined') {
     module.exports = _;
 }
