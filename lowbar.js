c65cbf2c58d15b740316de234d88374482493fa7
Kamran <kamran.mujtaba@hotmail.com>
2017-07-30 10:43:08 +0100

    _.each implemented and tested

--- lowbar.js
+++ lowbar.js
@@ -15,6 +15,18 @@ _.last = function (arr, num) {
      return arr.slice(-num);
 };
 
+_.each = function (list, func) {
+    if (Array.isArray(list)) {
+        for (var i = 0; i < list.length; i++) {
+            func(list[i], i);
+        }
+    } else if (typeof (list) === 'object') {
+        for (var key in list) {
+            func(list[key], key);
+        }
+    }
+    return list;
+};
 
 if (typeof module !== 'undefined') {
     module.exports = _;
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -53,4 +53,43 @@ describe('_', function() {
             expect(result).to.eql([{ name: 'wassan' }, { name: 'men' }]);
         });
     });
+    // each method
+    describe('#Each', function () {
+        it('is a function', function () {
+            expect(_.each).to.be.a('function');
+        });
+        it('calls the function as many times as items in the array', function () {
+            var count = 0;
+
+            function incrementCount() { count++; }
+            _.each([1, 2, 3], incrementCount);
+            expect(count).to.equal(3);
+        });
+        it('calls the function with each item of the array as the first argumet', function () {
+            var count = 0;
+           
+            function incrementCount() { count++; }
+            _.each([1, 2, 3], incrementCount);
+            expect(count).to.equal(3);
+        });
+        it('calls the function with each item of the array as the first argumet', function () {
+            var basket = [];
+
+            function putItemInBasket(item) {
+                basket.push(item);
+            }
+            _.each([1, 2, 3], putItemInBasket);
+            expect(basket).to.eql([1, 2, 3]);
+        });
+
+        it('calls the function with the index of each item as the second argumet', function () {
+            var basket = [];
+            function putIndexInBasket(item, index) {
+                basket.push(index);
+            }
+            _.each([1, 2, 3], putIndexInBasket);
+            expect(basket).to.eql([0, 1, 2]);
+        });
+
+    });
 });
