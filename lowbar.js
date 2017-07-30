baff5dd4019d08ddbe0d7cd17b58d6b025d7b6a1
Kamran <kamran.mujtaba@hotmail.com>
2017-07-30 11:21:07 +0100

    _.uniq implemented and tested

--- lowbar.js
+++ lowbar.js
@@ -75,6 +75,17 @@ _.reject = function (list, func) {
     return arr;
 };
 
+// uniq method
+_.uniq = function (arr) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (result.indexOf(arr[i]) === -1) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+};
+
 if (typeof module !== 'undefined') {
     module.exports = _;
 }
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -181,4 +181,22 @@ describe('_', function () {
             expect(result).to.eql([1, 2, 3, 4]);
         });
     });
+    // uniq method
+    describe('#uniq', function () {
+        it('is a function', function () {
+            expect(_.uniq).to.be.a('function');
+        });
+        it('it should return unique elements only', function () {
+            var result = _.uniq([1, 2, 3, 4, 5, 2, 4, 5]);
+            expect(result).to.eql([1, 2, 3, 4, 5]);
+        });
+        it('should return an array', function () {
+            var result = _.uniq([1, 2, 3, 4, 5]);
+            expect(result).to.be.an('array');
+        });
+        it('It should return an array of unique elements', function () {
+            var result = _.uniq(['a', 'b', 'c', 'c', 'b']);
+            expect(result).to.eql(['a', 'b', 'c']);
+        });
+    });
 });
